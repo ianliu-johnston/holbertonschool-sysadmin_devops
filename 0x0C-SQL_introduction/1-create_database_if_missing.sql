004e2c2a7c7fbd83240c1e0b65162cd97fea0c43
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-02-22 22:53:09 -0800

    Adding IF EXISTS and IF NOT EXISTS

--- 0x0C-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0C-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
 -- Creates a database.
-CREATE DATABASE hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
--- 0x0C-SQL_introduction/2-remove_database.sql
+++ 0x0C-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- Deletes a database.
-DROP DATABASE hbtn_0c_0
+DROP DATABASE IF EXISTS hbtn_0c_0
