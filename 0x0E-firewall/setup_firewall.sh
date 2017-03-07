427a3e0dedd6467b80f3a69a785b6428e7006bbb
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-07 15:45:30 -0800

    Task 1: Complete

--- 0x0E-firewall/setup_firewall.sh
+++ 0x0E-firewall/setup_firewall.sh
@@ -6,3 +6,4 @@ sudo ufw allow 8080
 sudo ufw allow 443
 sudo ufw allow 22
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080
+#ufw config found in /etc/ufw/rules.before
