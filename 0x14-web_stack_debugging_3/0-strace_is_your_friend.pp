79b0490ff962d9ca2745388df8f879e9d7628433
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-30 15:06:50 -0700

    Task 0: Forgot commas

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fixes a wordpress bug
 exec { 'fix_typo':
-  command => 'sed -i "s|.phpp|.php|g" /var/www/html/wp-settings.php'
-  path => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin']
+  command => 'sed -i "s|.phpp|.php|g" /var/www/html/wp-settings.php',
+  path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
 }
