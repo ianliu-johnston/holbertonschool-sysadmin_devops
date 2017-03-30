bfa3f937fe0ebda626b0121e1421155ae3de349a
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-30 14:33:14 -0700

    Task 2: Properly aligned the arrows

--- 0x13-configuration_management/2-execute_a_command.pp
+++ 0x13-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # kill a process
 exec { 'pkill_killmenow':
   command => 'pkill -f killmenow',
-  path => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games']
+  path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games'],
 }
