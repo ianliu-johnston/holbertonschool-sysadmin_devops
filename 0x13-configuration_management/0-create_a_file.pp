b35288d790478e18945fec518e67bf631d5db56b
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-30 13:34:25 -0700

    fixed puppet stfixed puppet style

--- 0x13-configuration_management/0-create_a_file.pp
+++ 0x13-configuration_management/0-create_a_file.pp
@@ -4,6 +4,6 @@ file { '/tmp/holberton':
   path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
-  group	  => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
