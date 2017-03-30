0879a9a5ce3be29740fccdcd2387c822b889238e
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-30 13:32:11 -0700

    Replaced all double quotes with single quotes

--- 0x13-configuration_management/0-create_a_file.pp
+++ 0x13-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Creates a new file
 file { '/tmp/holberton':
   ensure  => file,
-  path    => "/tmp/holberton",
-  mode    => "0744",
-  owner   => "www-data",
-  group	  => "www-data",
-  content => "I love Puppet",
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group	  => 'www-data',
+  content => 'I love Puppet',
 }
