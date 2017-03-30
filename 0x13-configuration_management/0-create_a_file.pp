fc065c192728580c6089e9e0615ae097975b9361
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-03-30 13:11:04 -0700

    Task 0: Fixed whitespace

--- 0x13-configuration_management/0-create_a_file.pp
+++ 0x13-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# p
+# lalalal
 file {
-	ensure	=> file,
-	path	=> "/tmp/holberton",
-	mode	=> "0744",
-	owner	=> "www-data",
-	group	=> "www-data",
-	content	=> "I love Puppet"
+  ensure  => file,
+  path    => "/tmp/holberton",
+  mode    => "0744",
+  owner   => "www-data",
+  group	  => "www-data",
+  content => "I love Puppet"
 }
