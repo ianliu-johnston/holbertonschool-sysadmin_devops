bd18005b7522e7ec449b9b47ce2b8cce5c25bf90
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-17 15:24:43 -0700

    Task 0: Complete

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
 """
-Documentation
+Gets the completed todo list for the user at id.
+Usage: ./0-gather_data_from_an_API.py 2
+where 2 is the user id of Ervin Howell.
+Fake data from "https://jsonplaceholder.typicode.com"
 """
 import requests
+from sys import argv
 
 
 if __name__ == "__main__":
-    print("Working")
+    root = "https://jsonplaceholder.typicode.com"
+    users = requests.get(root + "/users", params={"id" : argv[1]})
+    for names in users.json():
+        usr_id = names.get('id')
+        todo = requests.get(root + "/todos", params={"userId" : usr_id })
+        task_complete = 0
+        tasks_array = []
+        for tasks in todo.json():
+            if tasks.get('completed') == True:
+                task_complete += 1
+                tasks_array.append(tasks.get('title'))
+        print("Employee {:s} is done with tasks({:d}/{:d}):\n\t{}".
+                format(names.get('name'), task_complete, len(todo.json()), "\n\t".join(tasks_array)))
