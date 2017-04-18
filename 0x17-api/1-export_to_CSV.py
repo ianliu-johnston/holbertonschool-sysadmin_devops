69085ca19781e695305303ae34db0535f90df179
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-17 17:09:59 -0700

    Task 1: Fixed style

--- 0x17-api/1-export_to_CSV.py
+++ 0x17-api/1-export_to_CSV.py
@@ -12,14 +12,14 @@ import csv
 
 if __name__ == "__main__":
     root = "https://jsonplaceholder.typicode.com"
-    users = requests.get(root + "/users", params={"id" : argv[1]})
+    users = requests.get(root + "/users", params={"id": argv[1]})
     for names in users.json():
         usr_id = names.get('id')
-        todo = requests.get(root + "/todos", params={"userId" : usr_id })
+        todo = requests.get(root + "/todos", params={"userId": usr_id})
         csv_arr = []
         with open(argv[1] + ".csv", 'a') as f:
             writer = csv.writer(f, quoting=csv.QUOTE_ALL)
             for tasks in todo.json():
-                writer.writerow([names.get('id'),names.get('name'),
-                               str(tasks.get('completed')), tasks.get('title')
-                               ])
+                writer.writerow([names.get('id'), names.get('name'),
+                                 str(tasks.get('completed')),
+                                 tasks.get('title')])
