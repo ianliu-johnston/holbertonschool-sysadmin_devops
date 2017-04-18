2336c7575a8a9512e230db571f41d383dd1043da
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-17 17:04:24 -0700

    Task 2: Complete

--- 0x17-api/1-export_to_CSV.py
+++ 0x17-api/1-export_to_CSV.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
 """
-Documentation
+Gets the completed todo list for the user at id and prints to a CSV file.
+Usage: ./1-export_to_CSV.py 2
+where 2 is a user id
+Fake data from "https://jsonplaceholder.typicode.com"
 """
 import requests
+from sys import argv
+import csv
 
 
 if __name__ == "__main__":
-    print("Working")
+    root = "https://jsonplaceholder.typicode.com"
+    users = requests.get(root + "/users", params={"id" : argv[1]})
+    for names in users.json():
+        usr_id = names.get('id')
+        todo = requests.get(root + "/todos", params={"userId" : usr_id })
+        csv_arr = []
+        with open(argv[1] + ".csv", 'a') as f:
+            writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+            for tasks in todo.json():
+                writer.writerow([names.get('id'),names.get('name'),
+                               str(tasks.get('completed')), tasks.get('title')
+                               ])
