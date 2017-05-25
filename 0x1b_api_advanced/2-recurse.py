a6af1dccbd367d904421c1919e7af7323ba62960
ianliu-johnston <ian.liu-johnson@holbertonschool.com>
2017-05-24 23:25:36 -0700

    Task 2: Added try, except block

--- 0x1b_api_advanced/2-recurse.py
+++ 0x1b_api_advanced/2-recurse.py
@@ -15,10 +15,13 @@ def recurse(subreddit, after=None, all_results=[]):
     url = 'https://reddit.com/r/' + subreddit + '/hot/.json'
     headers = {'User-Agent': "lala"}
     r = requests.get(url, headers=headers, params=param)
-    new_after = r.json()['data'].get('after')
-    for data in r.json()['data'].get('children'):
-        all_results.append(data['data'].get('title'))
-    if new_after is not None:
-        return(recurse(subreddit, new_after, all_results))
-    else:
-        return(all_results)
+    try:
+        new_after = r.json()['data'].get('after')
+        for data in r.json()['data'].get('children'):
+            all_results.append(data['data'].get('title'))
+        if new_after is not None:
+            return(recurse(subreddit, new_after, all_results))
+        else:
+            return(all_results)
+    except:
+        return(None)
