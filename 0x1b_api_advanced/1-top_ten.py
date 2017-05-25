e0c92109ac6b573c611c9ac4bcf715a9a7ef51d3
ianliu-johnston <ian.liu-johnson@holbertonschool.com>
2017-05-24 22:02:03 -0700

    Outputs only 10 results

--- 0x1b_api_advanced/1-top_ten.py
+++ 0x1b_api_advanced/1-top_ten.py
@@ -9,11 +9,17 @@ def top_ten(subreddit):
     """
     Uses the reddit api to get the numbers of hot posts in a given subreddit
     """
+    new_lst = []
+    count = 0
     url = 'https://reddit.com/r/' + subreddit + '/hot/.json'
     headers = {'User-Agent': "lala"}
     r = requests.get(url, headers=headers)
     try:
-        for data in r['data'].get('children'):
-            print(data['data'].get('title'))
-    except:
+        for data in r.json()['data'].get('children'):
+            new_lst.append(data['data'].get('title'))
+            count += 1
+            if count > 11:
+                break
+        print("\n".join(x for x in new_lst))
+    except Exception as err:
         print("None")
