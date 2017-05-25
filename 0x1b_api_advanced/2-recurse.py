2881123da12d461498bb785b757df7a6a38aa4f0
ianliu-johnston <ian.liu-johnson@holbertonschool.com>
2017-05-24 23:14:38 -0700

    Task 2: Complete

---
+++ 0x1b_api_advanced/2-recurse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import requests
+"""
+Module to interface with the reddit api
+"""
+
+
+def recurse(subreddit, after=None, all_results=[]):
+    """
+    Uses the reddit api to get the numbers of hot posts in a given subreddit
+    """
+    param = {}
+    if after is not None:
+        param = {'after': after}
+    url = 'https://reddit.com/r/' + subreddit + '/hot/.json'
+    headers = {'User-Agent': "lala"}
+    r = requests.get(url, headers=headers, params=param)
+    new_after = r.json()['data'].get('after')
+    for data in r.json()['data'].get('children'):
+        all_results.append(data['data'].get('title'))
+    if new_after is not None:
+        return(recurse(subreddit, new_after, all_results))
+    else:
+        return(all_results)
